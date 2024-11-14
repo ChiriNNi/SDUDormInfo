567080bd6632c184d2bfa57fa64f4bfc6edc9ae5
ars <arsensejtkaliev@MacBook-Air-Arsen.local>
2024-11-14 13:35:38 +0500

    Работает тупо, но теперь можно язык выбрать

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
+import os
 import asyncio
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher, F
 from aiogram import Router
 from aiogram.fsm.context import FSMContext
 from models import StudentInfo
 from views import *
+from dotenv import load_dotenv
 
-
-bot = Bot(token='7371152325:AAFCtleaVO1iCbcoHcmyVBV1DiOnMelazDE')
-dp = Dispatcher()
+load_dotenv()
+storage = MemoryStorage()
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher(storage=storage)
 router = Router()
 
 
 @router.message(F.text == "/start")
-async def cmd_start(message: Message, state: FSMContext):
-    await message.answer(start().read(), reply_markup=payment_button())
-    await message.answer('Напишите полное имя: ')
+async def language_choice(message: Message, state: FSMContext):
+    await message.answer("Тілді таңдаңыз/Выберите язык", reply_markup=language_button())
+
+    await state.set_state(StudentInfo.language)
+
+
+@dp.callback_query(lambda c: c.data in ['ru', 'kz'])
+async def cmd_start(callback: CallbackQuery, state: FSMContext):
+    StudentInfo.language = callback.data
+    await callback.message.answer(start(StudentInfo.language).read())
+    await callback.message.answer("Толық аты-жөніңізді жазыңыз" if StudentInfo.language == "kz" else "Напишите полное имя")
     await state.set_state(StudentInfo.waiting_for_full_name)
 
 
 @router.message(StudentInfo.waiting_for_full_name)
 async def process_full_name(message: Message, state: FSMContext):
     user_name = message.text
-    print(f"Student id: {message.chat.id} \n Student full_name: {user_name}\n")
-    await message.answer("Напишите свой город: ")
+    data = await state.get_data()
+    lang = data.get("language")
+    print(f"Student id: {message.chat.id} \n Student full_name: {user_name}\n {StudentInfo.language}")
+    await message.answer("Қалаңызды жазыңыз" if StudentInfo.language == "kz" else "Напишите свой город")
     await state.set_state(StudentInfo.waiting_for_city)
 
 
@@ -31,7 +45,7 @@ async def process_city(message: Message, state: FSMContext):
     city = message.text
     print(f"Student id: {message.chat.id} \n Student city: {city}\n")
 
-    await message.answer("Напишите свой курс")
+    await message.answer("Курсыңызды жазыңыз" if StudentInfo.language == "kz" else "Напишите свой курс")
     await state.set_state(StudentInfo.waiting_for_course)
 
 
@@ -40,7 +54,7 @@ async def process_course(message: Message, state: FSMContext):
     course = message.text
     print(f"Student id: {message.chat.id} \n Student course: {course}\n")
 
-    await message.answer("Напишите свою специальность")
+    await message.answer("Мамандығыңызды жазыңыз" if StudentInfo.language == "kz" else "Напишите свою специальность")
     await state.set_state(StudentInfo.waiting_for_speciality)
 
 
@@ -49,22 +63,28 @@ async def process_speciality(message: Message, state: FSMContext):
     speciality = message.text
     print(f"Student id: {message.chat.id} \n Student speciality: {speciality}\n")
 
-    await message.answer("Выберите пол", reply_markup=gender_button())
+    await message.answer("Жыныс" if StudentInfo.language == "kz" else "Выберите пол", reply_markup=gender_button())
     await state.set_state(StudentInfo.waiting_for_gender)
 
     await state.clear()
 
 
-@dp.callback_query(F.data == 'option_1')
-async def handle_1(callback: CallbackQuery):
-    choice = "Мужчина"
-    print(f"Student id: {callback.message.chat.id} \n Student gender: {choice}\n")
+@router.message(StudentInfo.waiting_for_room)
+@dp.callback_query(lambda c: c.data in ["Мужчина", "Женщина"])
+async def handle_gender(callback: CallbackQuery, state: FSMContext):
+    print(f"Student id: {callback.message.chat.id} \n Student gender: {callback.data}\n")
+
+    await bot.send_message(callback.message.chat.id,"Бөлме таңдаңыз" if StudentInfo.language == "kz" else "Выберите комнату", reply_markup=rooms())
+    await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
+
+    await state.set_state(StudentInfo.waiting_for_room)
+    await state.clear()
 
 
-@dp.callback_query(F.data == 'option_2')
-async def handle_2(callback: CallbackQuery):
-    choice = "Женщина"
-    print(f"Student id: {callback.message.chat.id} \nStudent gender: {choice}")
+@dp.callback_query(F.data == 'room')
+async def handle_room(callback: CallbackQuery):
+    room = callback.message
+    print(room)
 
 
 dp.include_router(router)
--- views.py
+++ views.py
@@ -2,8 +2,16 @@ from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, C
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def start():
-    start_message = open('messages/start_message.txt')
+def language_button():
+    language = InlineKeyboardBuilder()
+    language.add(InlineKeyboardButton(text="KZ", callback_data="kz"))
+    language.add(InlineKeyboardButton(text="RU", callback_data="ru"))
+    reply_markup = language.as_markup()
+    return reply_markup
+
+
+def start(language):
+    start_message = open(f'messages/start_message_{language}.txt')
 
     return start_message
 
@@ -11,18 +19,28 @@ def start():
 def payment_button():
     keyboard_builder = InlineKeyboardBuilder()
     keyboard_builder.add(
-        InlineKeyboardButton(text="ЖАТАХАНА ҮШІН", url="https://ru.wikipedia.org/wiki/%D0%A0%D0%B5%D0%BA%D1%83%D1%80%D1%81%D0%B8%D1%8F"),
-        InlineKeyboardButton(text="ТАМАҚТАНУ ҮШІН", url="https://ru.wikipedia.org/wiki/%D0%A0%D0%B5%D0%BA%D1%83%D1%80%D1%81%D0%B8%D1%8F"),
+        InlineKeyboardButton(text="ЖАТАХАНА ҮШІН", url="https://kaspi.kz/pay/_gate?action=service_with_subservice&service_id=2766&subservice_id=12178&region_id=18"),
+        InlineKeyboardButton(text="ТАМАҚТАНУ ҮШІН", url="https://kaspi.kz/pay/_gate?action=service_with_subservice&service_id=2766&subservice_id=21871&region_id=18"),
     )
-    reply_markup=keyboard_builder.as_markup()
+    reply_markup = keyboard_builder.as_markup()
 
     return reply_markup
 
 
 def gender_button():
     gender = InlineKeyboardBuilder()
-    gender.add(InlineKeyboardButton(text="Мужчина", callback_data="option_1"))
-    gender.add(InlineKeyboardButton(text="Женщина", callback_data="option_2"))
-    reply_markup=gender.as_markup()
+    gender.add(InlineKeyboardButton(text="Мужчина", callback_data="Мужчина"))
+    gender.add(InlineKeyboardButton(text="Женщина", callback_data="Женщина"))
+    reply_markup = gender.as_markup()
 
     return reply_markup
+
+
+def rooms():
+    room = InlineKeyboardBuilder()
+    for i in range(10):
+        room.add(InlineKeyboardButton(text=f"C{322+i}", callback_data=f"room"))
+    room.adjust(2)
+    reply_markup = room.as_markup()
+    return reply_markup
+
